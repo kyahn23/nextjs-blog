4409e179102b35c719fa2bf24dc35f83be5eb624
kyahn23 <kyahn23@gmail.com>
2023-02-13 17:41:06 +0900

    markdown ing

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,22 +3,59 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import React from "react";
+import Image from "next/image";
+// import { language } from "gray-matter";
 
-// content : markdown 참고
-const DUMMY = {
-  slug: "getting-started-with-nextjs",
-  title: "getting-started-with-nextjs",
-  image: "getting-started-nextjs.png",
-  date: "2023-02-10",
-  content: "# This is a first post",
-};
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 function PostContent(props) {
-  const imagePath = `/images/posts/${DUMMY.slug}/${DUMMY.image}`;
+  const { post } = props;
+
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      // const { language, value } = code;
+      const { className, children } = code;
+      const language = className.split("-")[1];
+
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
